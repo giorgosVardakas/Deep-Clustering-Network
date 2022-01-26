54704459ba0b8265c47b309611c834f45ae651ad
Giorgos Vardakas <geoo1995@gmail.com>
2022-01-26 14:23:57 +0200

    Fashion-Mnist

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -166,6 +166,7 @@ def get_synthetic_dataloader(batch_size=64):
 
 	return dataloader, data_shape
 
+# TODO
 def get_newsgroups_dataloader(batch_size=64, data_points=-1):
 	#pdb.set_trace()
 	vectorizer = TfidfVectorizer()
