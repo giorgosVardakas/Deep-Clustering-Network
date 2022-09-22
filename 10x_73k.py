d173aa549d3bca34ee654cc97777a39fa2bc23f1
Giorgos Vardakas <geoo1995@gmail.com>
2022-09-22 17:43:45 +0300

    Update

--- 10x_73k.py
+++ 10x_73k.py
@@ -6,8 +6,33 @@ from torchvision import datasets, transforms
 from sklearn.metrics import adjusted_rand_score, accuracy_score
 from sklearn.metrics import normalized_mutual_info_score
 from torch.utils.data import Subset
-from datasets.datasets import get_10x_73k_dataloader
+from datasets.datasets import get_10x_73k
+from typing import Optional
+from scipy.optimize import linear_sum_assignment as linear_assignment
 import pdb
+
+def cluster_accuracy(y_true, y_predicted, cluster_number: Optional[int] = None):
+	"""
+	Calculate clustering accuracy after using the linear_sum_assignment function in SciPy to
+	determine reassignments.
+
+	:param y_true: list of true cluster numbers, an integer array 0-indexed
+	:param y_predicted: list of predicted cluster numbers, an integer array 0-indexed
+	:param cluster_number: number of clusters, if None then calculated from input
+	:return: reassignment dictionary, clustering accuracy
+	"""
+	if cluster_number is None:
+		# assume labels are 0-indexed
+		cluster_number = (max(y_predicted.max(), y_true.max()) + 1)
+	count_matrix = np.zeros((cluster_number, cluster_number), dtype=np.int64)
+	for i in range(y_predicted.size):
+		count_matrix[y_predicted[i], y_true[i]] += 1
+
+	row_ind, col_ind = linear_assignment(count_matrix.max() - count_matrix)
+	reassignment = dict(zip(row_ind, col_ind))
+	accuracy = count_matrix[row_ind, col_ind].sum() / y_predicted.size
+	return reassignment, accuracy
+
 
 def transform_clusters_to_labels(clusters, labels):
 	# Find the cluster ids (labels)
@@ -46,14 +71,17 @@ def evaluate(model, train_loader):
 	clusters = np.vstack(clusters).reshape(-1)
 	predicted_labels = transform_clusters_to_labels(clusters, labels)
 
-	ACC = accuracy_score(labels, predicted_labels)
+	labels = labels.astype("int")
+	ACC = cluster_accuracy(labels, clusters)[1]
+	PURITY = accuracy_score(labels, predicted_labels)
 	NMI = normalized_mutual_info_score(labels, clusters)
 	ARI = adjusted_rand_score(labels, clusters)
-	return (ACC, NMI, ARI)
+	return (ACC, PURITY, NMI, ARI)
 
 
 def solver(args, model, train_loader):
 	rec_loss_list = model.pretrain(train_loader, args.pre_epoch)
+	pur_list = []
 	acc_list = []
 	nmi_list = []
 	ari_list = []
@@ -63,14 +91,15 @@ def solver(args, model, train_loader):
 		model.fit(e, train_loader)
 
 		model.eval()
-		ACC, NMI, ARI = evaluate(model, train_loader)  # evaluation on test_loader
+		ACC, PURITY, NMI, ARI = evaluate(model, train_loader)  # evaluation on test_loader
 		acc_list.append(ACC)
+		pur_list.append(PURITY)
 		nmi_list.append(NMI)
 		ari_list.append(ARI)
 
-		print('Epoch: {:02d} | ACC: {:.3f} | NMI: {:.3f} | ARI: {:.3f}\n'.format(e, ACC, NMI, ARI))
+		print('Epoch: {:02d} | ACC: {:.2f} | PURITY: {:.2f} | NMI: {:.2f} | ARI: {:.2f}\n'.format(e, ACC, PURITY, NMI, ARI))
 
-	return rec_loss_list, acc_list, nmi_list, ari_list
+	return rec_loss_list, acc_list, pur_list, nmi_list, ari_list
 
 
 if __name__ == '__main__':
@@ -83,28 +112,28 @@ if __name__ == '__main__':
 	# Training parameters
 	parser.add_argument('--lr', type=float, default=0.002, help='learning rate (default: 1e-4)')
 	parser.add_argument('--wd', type=float, default=5e-4, help='weight decay (default: 5e-4)')
-	parser.add_argument('--batch-size', type=int, default=128, help='input batch size for training')
+	parser.add_argument('--batch-size', type=int, default=256, help='input batch size for training')
 	parser.add_argument('--epoch', type=int, default=50, help='number of epochs to train')
-	parser.add_argument('--pre-epoch', type=int, default=50, help='number of pre-train epochs')
+	parser.add_argument('--pre-epoch', type=int, default=300, help='number of pre-train epochs')
 	parser.add_argument('--pretrain', type=bool, default=True, help='whether use pre-training')
 	
 	# Model parameters
 	parser.add_argument('--lamda', type=float, default=0.005, help='coefficient of the reconstruction loss')
 	parser.add_argument('--beta', type=float, default=1, help='coefficient of the regularization term on clustering')
-	parser.add_argument('--hidden-dims', default=[500, 500, 1000], help='learning rate (default: 1e-4)')
+	parser.add_argument('--hidden-dims', default=[50], help='learning rate (default: 1e-4)')
 	parser.add_argument('--latent-dim', type=int, default=10, help='latent space dimension')
 	parser.add_argument('--n-clusters', type=int, default=8, help='number of clusters in the latent space')
 	parser.add_argument('--clustering', type=str, default='kmeans', help='choose a clustering method (default: kmeans) meanshift, tba')
 
 	# Utility parameters
 	parser.add_argument('--n-jobs', type=int, default=1, help='number of jobs to run in parallel')
-	parser.add_argument('--device', type=str, default='cuda', help='device for computation (default: cpu)')
+	parser.add_argument('--device', type=str, default='cuda', help='device for computation (default: cuda)')
 
 	args = parser.parse_args()
 
 	# Load data
-	train_loader, datashape = get_10x_73k_dataloader(args.batch_size, data_points=70000)
+	train_loader, datashape = get_10x_73k(args.batch_size, -1)
 	
 	# Main body
 	model = DCN(args)    
-	rec_loss_list, acc_list, nmi_list, ari_list = solver(args, model, train_loader)
+	rec_loss_list, acc_list, pur_list, nmi_list, ari_list = solver(args, model, train_loader)
