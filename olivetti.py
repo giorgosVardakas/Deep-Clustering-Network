553f357fbf079994aeadce9363da10b5b5fa9e90
Giorgos Vardakas <geoo1995@gmail.com>
2022-09-22 18:19:36 +0300

    Update

--- olivetti.py
+++ olivetti.py
@@ -6,7 +6,7 @@ from torchvision import datasets, transforms
 from sklearn.metrics import adjusted_rand_score, accuracy_score
 from sklearn.metrics import normalized_mutual_info_score
 from torch.utils.data import Subset
-from datasets.datasets import get_FashionMNIST_dataloader
+from datasets.datasets import get_olivetti_faces_dataloader
 from typing import Optional
 from scipy.optimize import linear_sum_assignment as linear_assignment
 import pdb
@@ -78,7 +78,8 @@ def evaluate(model, train_loader):
 	ARI = adjusted_rand_score(labels, clusters)
 	return (ACC, PURITY, NMI, ARI)
 
-def solver(args, model, train_loader, test_loader):
+
+def solver(args, model, train_loader):
 	rec_loss_list = model.pretrain(train_loader, args.pre_epoch)
 	pur_list = []
 	acc_list = []
@@ -90,7 +91,7 @@ def solver(args, model, train_loader, test_loader):
 		model.fit(e, train_loader)
 
 		model.eval()
-		ACC, PURITY, NMI, ARI = evaluate(model, test_loader)  # evaluation on test_loader
+		ACC, PURITY, NMI, ARI = evaluate(model, train_loader)  # evaluation on test_loader
 		acc_list.append(ACC)
 		pur_list.append(PURITY)
 		nmi_list.append(NMI)
@@ -100,45 +101,39 @@ def solver(args, model, train_loader, test_loader):
 
 	return rec_loss_list, acc_list, pur_list, nmi_list, ari_list
 
+
 if __name__ == '__main__':
 	
 	parser = argparse.ArgumentParser(description='Deep Clustering Network')
 
 	# Dataset parameters
-	parser.add_argument('--input-dim', type=int, default=28*28, help='input dimension')
+	parser.add_argument('--input-dim', type=int, default=4096, help='input dimension')
 
 	# Training parameters
-	parser.add_argument('--lr', type=float, default=0.002, help='learning rate (default: 1e-4)')
+	parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (default: 1e-4)')
 	parser.add_argument('--wd', type=float, default=5e-4, help='weight decay (default: 5e-4)')
-	parser.add_argument('--batch-size', type=int, default=256, help='input batch size for training')
+	parser.add_argument('--batch-size', type=int, default=40, help='input batch size for training')
 	parser.add_argument('--epoch', type=int, default=50, help='number of epochs to train')
-	parser.add_argument('--pre-epoch', type=int, default=5, help='number of pre-train epochs')
-
+	parser.add_argument('--pre-epoch', type=int, default=1500, help='number of pre-train epochs')
 	parser.add_argument('--pretrain', type=bool, default=True, help='whether use pre-training')
 	
 	# Model parameters
 	parser.add_argument('--lamda', type=float, default=0.005, help='coefficient of the reconstruction loss')
 	parser.add_argument('--beta', type=float, default=1, help='coefficient of the regularization term on clustering')
-	parser.add_argument('--hidden-dims', default=[500, 500, 2000], help='learning rate (default: 1e-4)')
+	parser.add_argument('--hidden-dims', default=[500, 500, 1000], help='learning rate (default: 1e-4)')
 	parser.add_argument('--latent-dim', type=int, default=10, help='latent space dimension')
-	parser.add_argument('--n-clusters', type=int, default=10, help='number of clusters in the latent space')
-	parser.add_argument('--clustering', type=str, default='kmeans', help='choose a clustering method (default: kmeans)' \
-					   ' meanshift, tba')
-
+	parser.add_argument('--n-clusters', type=int, default=40, help='number of clusters in the latent space')
+	parser.add_argument('--clustering', type=str, default='kmeans', help='choose a clustering method (default: kmeans) meanshift, tba')
 
 	# Utility parameters
 	parser.add_argument('--n-jobs', type=int, default=1, help='number of jobs to run in parallel')
-	parser.add_argument('--device', type=str, default='cuda', help='device for computation (default: cpu)')
-	parser.add_argument('--log-interval', type=int, default=400, help='how many batches to wait before logging the ' \
-							'training status')
-	parser.add_argument('--test-run', action='store_true', help='short test run on a few instances of the dataset')
+	parser.add_argument('--device', type=str, default='cuda', help='device for computation (default: cuda)')
 
-	
 	args = parser.parse_args()
-	
-	# Load data
-	train_loader = get_FashionMNIST_dataloader(args.batch_size)
 
+	# Load data
+	train_loader, datashape = get_olivetti_faces_dataloader(args.batch_size)
+	
 	# Main body
 	model = DCN(args)    
-	rec_loss_list, acc_list, pur_list, nmi_list, ari_list = solver(args, model, train_loader, train_loader)
+	rec_loss_list, acc_list, pur_list, nmi_list, ari_list = solver(args, model, train_loader)
